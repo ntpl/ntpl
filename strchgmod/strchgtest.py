32b63ebb0433c0c1533836d6cbc5df7f01444ea2
Kevin Parrish <kevin.dale.parrish@gmail.com>
2012-07-31 17:27:44 -0400

    ...fixed strchgtest file

--- strchgmod/strchgtest.py
+++ strchgmod/strchgtest.py
@@ -1,17 +1,19 @@
 ## ## ## This script will provide examples of how to implement
 ## ## ## the ntplpy.strchg module
 
-## First import the module
+ntplPATH = '/home/kevin/ntpl'
+
 ## Import the module
+import os
 import sys
-sys.path.append('/home/kevin/ntpl') # Needed to link the ntplpy module
+sys.path.append(ntplPATH) # Needed to link the ntplpy module
 import ntplpy.strchg as st
 
 ## ## We will use the sed command. First, create a dict of key value pairs.
-## ## Each key is an original bkey and each value is the new bkey.
+## ## Each key is an original string and each value is the new string.
 
 ##	First, create a key:value dict.
-mydict = dict({'key': 'key', 'bkey': 'bkey'})
+mydict = dict({'sed': 'MARIO', 'dict': 'LUIGI'})
 # The following methods are equivalent ways.
 # mydict = dict(TEMP_PARAM='80', STRAIN_PARAM='0.02')
 # mydict = dict(zip(('TEMP_PARAM', 'STRAIN_PARAM'), ('80', '0.02')))
@@ -22,12 +24,14 @@ st.sed(mydict, 'strchgtest.py', 'tmp.newfile')
 ## ## And you're done!!!
 
 ## If you want to edit the file in-place, i.e. not create a new file,
-## then simply make the new file name a blank bkey.
-st.sed(mydict, 'strchgtest.py', '')
+## then simply make the new file name a blank string.
+st.sed(mydict, 'tmp.newfile', '')
 
 ## You can also use relative or absolute pathways for the file names.
-mydict = dict({'key': 'key', 'bkey': 'bkey'})
-orig = '/home/kevin/ntpl/strchgmod/tmp.newfile'
-new = './strchgtest.py'
+mydict = dict({'sed': 'MARIO', 'dict': 'LUIGI'})
+orig = ntplPATH + '/strchgmod/strchgtest.py'
+new = './tmp.newfile'
 st.sed(mydict, orig, new)
 
+#cleanup
+os.system('rm tmp.newfile')
