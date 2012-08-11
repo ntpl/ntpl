002a04d6021470ae0b45c0e77a669e8952bdc36c
Samuel Huberman <samuel.huberman@gmail.com>
2012-08-11 13:14:22 -0400

    copied nmdpy from https://github.com/schuberm

--- Codes/ntpy/nmdpy/nmd.py
+++ Codes/ntpy/nmdpy/nmd.py
@@ -1,24 +1,29 @@
+#Author samuel.huberman@gmail.com
+
+import numpy as np
 
 class Nmd:
-	"""
-	Creates an nmd object for the nmd calculation.
-	nmdpy.Nmd(vel,pos)
-	Parameters
-	----------
-		vel : array
-			time series of atomic velocities from MD
-		pos : array
-			equilibrium atomic positions
-	"""
-	def __init__(self,
-			vel,
-			pos):
+
+	def __init__(self,vel,pos,eig,kpt):
 			self._vel=vel
 			self._pos=pos
+			self._eig=eig
+			self._kpt=kpt
+			self._nfft=vel.nvel()/pos.natoms()
+			self._natomucell=eig.natomucell()
+			self._nrep=self._nfft*pos.natoms()/self._natomucell
 
-	def spctEnrg(self,eigvec,k):
-		qdot=eigvec.x*self._vel.x+
-			eigvec.y*self._vel.y+
-			eigvec.y*self._vel.z
+	def autoCorr(x):
+		result = np.correlate(x, x, mode='full')
+		return result[result.size/2:]
 
+	def spctEnrg(self,kindex,mode):
+
+		spatial=2*np.complex(0,1)*np.pi*(self._pos.x()/self._pos.lx()*self._kpt[kindex,0]+
+			self._pos.y()/self._pos.ly()*self._kpt[kindex,1]+
+			self._pos.z()/self._pos.lz()*self._kpt[kindex,2])
+		qdot=np.exp(spatial)*(np.tile(self._eig.x(kindex,mode),(self._nrep,1)).flatten(1)*self._vel.vxts()+
+		np.tile(self._eig.y(kindex,mode),(self._nrep,1)).flatten(1)*self._vel.vyts()+
+		np.tile(self._eig.z(kindex,mode),(self._nrep,1)).flatten(1)*self._vel.vzts())
+		return np.fft.fft(np.correlate(qdot,qdot,mode='full'))
 
